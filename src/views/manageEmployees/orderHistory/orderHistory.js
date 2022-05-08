a4073cb3cc3f4418fa48bcf74f0eb0a043947ed5
Jawwad <mjawwad072@gmail.com>
2022-05-08 22:51:49 +0500

    Order Table Filteration

--- src/views/manageEmployees/orderHistory/orderHistory.js
+++ src/views/manageEmployees/orderHistory/orderHistory.js
@@ -24,6 +24,9 @@ const ip = process.env.REACT_APP_ADDR
 
 function Orderhistory() {
   const [data, setData] = useState([])
+  const [stat, setStat] = useState('all')
+  let ctabbod
+
   useEffect(() => {
     fetch(`http://` + ip + `:5000/api/orders`).then((result) => {
       result.json().then((resp) => {
@@ -50,6 +53,40 @@ function Orderhistory() {
     return () => clearInterval(interval)
   }, [])
 
+  ctabbod = data.map((items, index) => {
+    if (stat === 'all') {
+      return (
+        <CTableRow key={index}>
+          <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
+          <CTableDataCell> {items.order_time} </CTableDataCell>
+          <CTableDataCell> {items.total_price} </CTableDataCell>
+          <CTableDataCell> {items.payment_mode} </CTableDataCell>
+          <CTableDataCell> {items.rating} </CTableDataCell>
+          <CTableDataCell> {items.review} </CTableDataCell>
+          <CTableDataCell> {items.table_id} </CTableDataCell>
+          <CTableDataCell> {items.users_name} </CTableDataCell>
+          <CTableDataCell> {items.order_status} </CTableDataCell>
+        </CTableRow>
+      )
+    } else {
+      if (items.order_status === stat) {
+        return (
+          <CTableRow key={index}>
+            <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
+            <CTableDataCell> {items.order_time} </CTableDataCell>
+            <CTableDataCell> {items.total_price} </CTableDataCell>
+            <CTableDataCell> {items.payment_mode} </CTableDataCell>
+            <CTableDataCell> {items.rating} </CTableDataCell>
+            <CTableDataCell> {items.review} </CTableDataCell>
+            <CTableDataCell> {items.table_id} </CTableDataCell>
+            <CTableDataCell> {items.users_name} </CTableDataCell>
+            <CTableDataCell> {items.order_status} </CTableDataCell>
+          </CTableRow>
+        )
+      }
+    }
+  })
+
   //const viewEmployee = () => {
   return (
     <>
@@ -71,31 +108,54 @@ function Orderhistory() {
                     <CTableHeaderCell scope="col">Table no</CTableHeaderCell>
                     <CTableHeaderCell scope="col">Chef</CTableHeaderCell>
                     <CTableHeaderCell scope="col">
-                      <select>
+                      <select
+                        onChange={(e) => {
+                          setStat(e.target.value)
+                        }}
+                      >
                         <option selected value="all">
                           All
                         </option>
+                        <option value="n assg">Not Assigned</option>
+                        <option value="assigned">Assigned</option>
                         <option value="ready">Ready</option>
-                        <option value="ready">Assigned</option>
-                        <option value="ready">Not Assigned</option>
+                        <option value="serving">Serving</option>
+                        <option value="served">Served</option>
                       </select>
                     </CTableHeaderCell>
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  {data.map((items, index) => (
-                    <CTableRow key={index}>
-                      <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
-                      <CTableDataCell> {items.order_time} </CTableDataCell>
-                      <CTableDataCell> {items.total_price} </CTableDataCell>
-                      <CTableDataCell> {items.payment_mode} </CTableDataCell>
-                      <CTableDataCell> {items.rating} </CTableDataCell>
-                      <CTableDataCell> {items.review} </CTableDataCell>
-                      <CTableDataCell> {items.table_id} </CTableDataCell>
-                      <CTableDataCell> {items.users_name} </CTableDataCell>
-                      <CTableDataCell> {items.order_status} </CTableDataCell>
-                    </CTableRow>
-                  ))}
+                  {/* {data.map((items, index) => {
+                    if (stat === 'all') {
+                      <CTableRow key={index}>
+                        <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
+                        <CTableDataCell> {items.order_time} </CTableDataCell>
+                        <CTableDataCell> {items.total_price} </CTableDataCell>
+                        <CTableDataCell> {items.payment_mode} </CTableDataCell>
+                        <CTableDataCell> {items.rating} </CTableDataCell>
+                        <CTableDataCell> {items.review} </CTableDataCell>
+                        <CTableDataCell> {items.table_id} </CTableDataCell>
+                        <CTableDataCell> {items.users_name} </CTableDataCell>
+                        <CTableDataCell> {items.order_status} </CTableDataCell>
+                      </CTableRow>
+                    } else {
+                      if (items.order_status === stat) {
+                        <CTableRow key={index}>
+                          <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
+                          <CTableDataCell> {items.order_time} </CTableDataCell>
+                          <CTableDataCell> {items.total_price} </CTableDataCell>
+                          <CTableDataCell> {items.payment_mode} </CTableDataCell>
+                          <CTableDataCell> {items.rating} </CTableDataCell>
+                          <CTableDataCell> {items.review} </CTableDataCell>
+                          <CTableDataCell> {items.table_id} </CTableDataCell>
+                          <CTableDataCell> {items.users_name} </CTableDataCell>
+                          <CTableDataCell> {items.order_status} </CTableDataCell>
+                        </CTableRow>
+                      }
+                    }
+                  })} */}
+                  {ctabbod}
                 </CTableBody>
               </CTable>
             </CContainer>
